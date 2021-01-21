6bb40a865526fad1d1b796406d5d6fc6456eccb7
y724u <68385107+y724u@users.noreply.github.com>
2021-01-21 11:20:36 +0900

    余白を修正

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-$(function() {
-  
+$(function () {
+
 
 });
+
+var mySwiper = new Swiper('.swiper-container', {
+  // 1度に表示するスライドの数
+  slidesPerView:1,
+  // スライドの方向
+  direction: 'horizontal',
+  // スライドの間の余白
+  // spaceBetween: '40',
+  // ループモード
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
