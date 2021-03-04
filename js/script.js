9c59016eeb68553147e22bbbc0568674c6c5741c
y724u <68385107+y724u@users.noreply.github.com>
2021-03-04 16:52:02 +0900

    スムーススクロールを追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 $(function () {
 
+  // <!-- スムーススクロール -->
+  // #から始まるURLがクリックされた時
+  $('a[href^="#"]').on('click', function () {
+    // 移動速度を指定（ミリ秒）
+    let speed = 700;
+    // hrefで指定されたidを取得
+    let id = $(this).attr("href");
+    // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+    let target = $("#" == id ? "html" : id);
+    // ページのトップを基準にターゲットの位置を取得
+    let position = $(target).offset().top - 100;
+    // ターゲットの位置までspeedの速度で移動
+    $("html, body").animate(
+      {
+        scrollTop: position
+      },
+      speed
+    );
+    return false;
+  });
 
 });
 
